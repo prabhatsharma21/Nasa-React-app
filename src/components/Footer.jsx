5f78fb304197f1984e549f33a1b5ed81412e058c
prabhatsharma21 <prabhatsharmabest1@gmail.com>
2024-06-13 05:26:45 +0530

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 export default function Footer(props) {
-    const {showModal,handleToggleModal } = props
+    const {showModal,handleToggleModal, data } = props
 
     return (
         <footer>
             <div className="bgGradient"></div>
             <div>
-                <h2>The Brutal Martian Landscape</h2>
+                <h2>{data?.title}</h2>
                 <h1>APOD PROJECT</h1>
             </div>
             <button onClick={handleToggleModal}>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,14 +1,14 @@
 export default function SideBar(props) {
-    const { handleToggleModal } = props
+    const { handleToggleModal, data } = props
 
     return (
         <div className="sidebar">
             <div onClick={handleToggleModal} className="bgOverlay"></div>
             <div className="sidebarContents">
-            <h2>The Brutal Martian Landscape</h2>
-            <div>
-                <p>Description</p>
-                <p> nhjgrejhierhisrdhrhsgfdhfshjjjjjjjj hjsrdusdghr grfhrghrejgr huighjserjhge hniuvhsush </p>
+            <h2>{data?.title}</h2>
+            <div className="descriptionContainer">
+                <p className="descriptionTitle">{data?.date}</p>
+                <p>{data?.explanation}</p>
             </div>
             <button onClick={handleToggleModal} >
                <i className="fa-solid fa-arrow-right"></i>
